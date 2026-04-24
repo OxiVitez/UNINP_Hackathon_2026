4b56d8bba5a4d0fb25482defdcc216256bce5fae
Jovan Drmanic <jovandrmanic11@gmail.com>
2026-04-24 19:00:55 +0200

    soba tile manage

--- Scene/Sobe/RoomData.cs
+++ Scene/Sobe/RoomData.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Godot;
 
 [Serializable]
@@ -7,28 +8,36 @@ public partial class PlacedObjectData
     public string PlayerID { get; set; } = string.Empty;
     public string ObjectType { get; set; } = string.Empty;
     public Vector2I GridPosition { get; set; } = Vector2I.Zero;
+    public Vector2I SizeInTiles { get; set; } = Vector2I.One;
 
-    public Dictionary ToDictionary()
+    public Godot.Collections.Dictionary ToDictionary()
     {
-        return new Dictionary
+        return new Godot.Collections.Dictionary
         {
             { "player_id", PlayerID },
             { "object_type", ObjectType },
             { "grid_x", GridPosition.X },
-            { "grid_y", GridPosition.Y }
+            { "grid_y", GridPosition.Y },
+            { "size_x", SizeInTiles.X },
+            { "size_y", SizeInTiles.Y }
         };
     }
 
-    public static PlacedObjectData FromDictionary(Dictionary data)
+    public static PlacedObjectData FromDictionary(Godot.Collections.Dictionary data)
     {
+        var playerId = data.ContainsKey("player_id") ? data["player_id"].AsString() : string.Empty;
+        var objectType = data.ContainsKey("object_type") ? data["object_type"].AsString() : string.Empty;
+        var gridX = data.ContainsKey("grid_x") ? data["grid_x"].AsInt32() : 0;
+        var gridY = data.ContainsKey("grid_y") ? data["grid_y"].AsInt32() : 0;
+        var sizeX = data.ContainsKey("size_x") ? data["size_x"].AsInt32() : 1;
+        var sizeY = data.ContainsKey("size_y") ? data["size_y"].AsInt32() : 1;
+
         return new PlacedObjectData
         {
-            PlayerID = data.GetValueOrDefault("player_id", string.Empty).AsString(),
-            ObjectType = data.GetValueOrDefault("object_type", string.Empty).AsString(),
-            GridPosition = new Vector2I(
-                data.GetValueOrDefault("grid_x", 0).AsInt32(),
-                data.GetValueOrDefault("grid_y", 0).AsInt32()
-            )
+            PlayerID = playerId,
+            ObjectType = objectType,
+            GridPosition = new Vector2I(gridX, gridY),
+            SizeInTiles = new Vector2I(sizeX, sizeY)
         };
     }
 }
@@ -37,32 +46,33 @@ public partial class PlacedObjectData
 public partial class RoomData
 {
     public string PlayerID { get; set; } = string.Empty;
-    public Godot.Collections.Array<PlacedObjectData> Objects { get; set; } = new();
+    public List<PlacedObjectData> Objects { get; set; } = new();
 
-    public Dictionary ToDictionary()
+    public Godot.Collections.Dictionary ToDictionary()
     {
-        var objectsArray = new Godot.Collections.Array<Dictionary>();
+        var objectsArray = new Godot.Collections.Array<Godot.Collections.Dictionary>();
         foreach (var placedObject in Objects)
         {
             objectsArray.Add(placedObject.ToDictionary());
         }
 
-        return new Dictionary
+        return new Godot.Collections.Dictionary
         {
             { "player_id", PlayerID },
             { "objects", objectsArray }
         };
     }
 
-    public static RoomData FromDictionary(Dictionary data)
+    public static RoomData FromDictionary(Godot.Collections.Dictionary data)
     {
         var roomData = new RoomData
         {
-            PlayerID = data.GetValueOrDefault("player_id", string.Empty).AsString()
+            PlayerID = data.ContainsKey("player_id") ? data["player_id"].AsString() : string.Empty
         };
 
-        var objectsVariant = data.GetValueOrDefault("objects", new Godot.Collections.Array());
-        var objectsArray = objectsVariant.AsGodotArray();
+        var objectsArray = data.ContainsKey("objects")
+            ? data["objects"].AsGodotArray()
+            : new Godot.Collections.Array();
 
         foreach (var item in objectsArray)
         {
--- Scene/Sobe/RoomManager.cs
+++ Scene/Sobe/RoomManager.cs
@@ -1,16 +1,26 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class RoomManager : Node2D
 {
+    private sealed class InventoryItem
+    {
+        public string ObjectType { get; set; } = string.Empty;
+        public string TexturePath { get; set; } = string.Empty;
+        public Vector2I SizeInTiles { get; set; } = Vector2I.One;
+    }
+
     [Export] public TileMapLayer BaseGridLayer;
     [Export] public Node2D ObjectsContainer;
     [Export] public Vector2I CellSize = new(16, 16);
     [Export] public Godot.Collections.Dictionary<string, PackedScene> ObjectPrefabs = new();
     [Export] public string ActivePlayerId = "player_1";
-    [Export] public string ActiveObjectType = "TestObject";
 
-    private Vector2I _selectedSizeInTiles = Vector2I.One;
+    private readonly HashSet<Vector2I> _occupiedTiles = new();
+    private readonly List<InventoryItem> _inventoryItems = new();
+    private int _selectedItemIndex;
+    private Label _inventoryLabel;
 
     public override void _Ready()
     {
@@ -34,6 +44,10 @@ public partial class RoomManager : Node2D
         {
             BaseGridLayer.TileSet.TileSize = CellSize;
         }
+
+        SetupInventory();
+        BuildInventoryUi();
+        RebuildOccupiedTiles();
     }
 
     public override void _UnhandledInput(InputEvent @event)
@@ -60,7 +74,8 @@ public partial class RoomManager : Node2D
         );
 
         // Most Node2D content (Sprite2D) has centered pivot. Shift to the center of occupied grid area.
-        return snappedTopLeft + sizePixels / 2.0f;
+        var halfSize = new Vector2(sizePixels.X / 2.0f, sizePixels.Y / 2.0f);
+        return snappedTopLeft + halfSize;
     }
 
     public void PlaceObject(Node2D instance, string objectType, Vector2 globalPos, Vector2I sizeInTiles, string playerId)
@@ -73,6 +88,12 @@ public partial class RoomManager : Node2D
 
         var snappedPosition = GetSnappedPosition(globalPos, sizeInTiles);
         var gridPosition = GetGridPositionFromSnapped(snappedPosition, sizeInTiles);
+        if (!CanPlaceAt(gridPosition, sizeInTiles))
+        {
+            instance.QueueFree();
+            GD.Print("Cannot place item: target tiles are already occupied.");
+            return;
+        }
 
         instance.GlobalPosition = snappedPosition;
         instance.SetMeta("ObjectType", objectType);
@@ -84,6 +105,8 @@ public partial class RoomManager : Node2D
         {
             ObjectsContainer.AddChild(instance);
         }
+
+        MarkOccupied(gridPosition, sizeInTiles);
     }
 
     public void SaveRoom(string playerId)
@@ -103,16 +126,20 @@ public partial class RoomManager : Node2D
                 continue;
             }
 
-            var objectType = node.HasMeta("ObjectType") ? node.GetMeta("ObjectType").AsString() : node.Name;
+            var objectType = node.HasMeta("ObjectType") ? node.GetMeta("ObjectType").AsString() : node.Name.ToString();
             var gridPosition = node.HasMeta("GridPosition")
                 ? (Vector2I)node.GetMeta("GridPosition")
                 : WorldToGrid(node.GlobalPosition);
+            var sizeInTiles = node.HasMeta("SizeInTiles")
+                ? (Vector2I)node.GetMeta("SizeInTiles")
+                : Vector2I.One;
 
             roomData.Objects.Add(new PlacedObjectData
             {
                 PlayerID = playerId,
                 ObjectType = objectType,
-                GridPosition = gridPosition
+                GridPosition = gridPosition,
+                SizeInTiles = sizeInTiles
             });
         }
 
@@ -133,6 +160,7 @@ public partial class RoomManager : Node2D
         }
 
         ClearObjectsContainer();
+        _occupiedTiles.Clear();
 
         var savePath = GetRoomFilePath(playerId);
         if (!FileAccess.FileExists(savePath))
@@ -155,19 +183,20 @@ public partial class RoomManager : Node2D
 
         foreach (var entry in roomData.Objects)
         {
-            var node = InstantiateObjectForType(entry.ObjectType);
+            var node = InstantiateObjectForType(entry.ObjectType, entry.SizeInTiles);
             if (node == null)
             {
                 continue;
             }
 
-            var position = GridToWorld(entry.GridPosition, Vector2I.One);
+            var position = GridToWorld(entry.GridPosition, entry.SizeInTiles);
             node.GlobalPosition = position;
             node.SetMeta("ObjectType", entry.ObjectType);
             node.SetMeta("GridPosition", entry.GridPosition);
-            node.SetMeta("SizeInTiles", Vector2I.One);
+            node.SetMeta("SizeInTiles", entry.SizeInTiles);
             node.SetMeta("PlayerID", entry.PlayerID);
             ObjectsContainer.AddChild(node);
+            MarkOccupied(entry.GridPosition, entry.SizeInTiles);
         }
     }
 
@@ -176,20 +205,19 @@ public partial class RoomManager : Node2D
         switch (keycode)
         {
             case Key.Key1:
-                _selectedSizeInTiles = new Vector2I(1, 1);
-                GD.Print("Selected size: 1x1 (16x16)");
+                SelectInventoryItem(0);
                 break;
             case Key.Key2:
-                _selectedSizeInTiles = new Vector2I(2, 2);
-                GD.Print("Selected size: 2x2 (32x32)");
+                SelectInventoryItem(1);
                 break;
             case Key.Key3:
-                _selectedSizeInTiles = new Vector2I(4, 4);
-                GD.Print("Selected size: 4x4 (64x64)");
+                SelectInventoryItem(2);
                 break;
             case Key.Key4:
-                _selectedSizeInTiles = new Vector2I(8, 8);
-                GD.Print("Selected size: 8x8 (128x128)");
+                SelectInventoryItem(3);
+                break;
+            case Key.Tab:
+                SelectInventoryItem((_selectedItemIndex + 1) % _inventoryItems.Count);
                 break;
             case Key.F5:
                 SaveRoom(ActivePlayerId);
@@ -204,41 +232,50 @@ public partial class RoomManager : Node2D
 
     private void PlaceTestObjectAtMouse()
     {
-        var mousePosition = GetGlobalMousePosition();
-        var node = InstantiateObjectForType(ActiveObjectType);
-        if (node == null)
+        if (_inventoryItems.Count == 0)
         {
-            GD.PushError($"Could not instantiate object type '{ActiveObjectType}'.");
+            GD.PushError("Inventory is empty.");
             return;
         }
 
-        // When no prefab exists, create a simple colored square marker for quick visual testing.
-        if (node is Node2D node2D && node2D.GetChildCount() == 0 && !ObjectPrefabs.ContainsKey(ActiveObjectType))
+        var selectedItem = _inventoryItems[_selectedItemIndex];
+        var mousePosition = GetGlobalMousePosition();
+        var node = InstantiateObjectForType(selectedItem.ObjectType, selectedItem.SizeInTiles);
+        if (node == null)
         {
-            var marker = new ColorRect
-            {
-                Color = new Color(0.2f, 0.75f, 1.0f, 0.65f),
-                Size = new Vector2(_selectedSizeInTiles.X * CellSize.X, _selectedSizeInTiles.Y * CellSize.Y),
-                Position = new Vector2(
-                    -(_selectedSizeInTiles.X * CellSize.X) / 2.0f,
-                    -(_selectedSizeInTiles.Y * CellSize.Y) / 2.0f
-                )
-            };
-            node2D.AddChild(marker);
+            GD.PushError($"Could not instantiate object type '{selectedItem.ObjectType}'.");
+            return;
         }
 
-        PlaceObject(node, ActiveObjectType, mousePosition, _selectedSizeInTiles, ActivePlayerId);
+        PlaceObject(node, selectedItem.ObjectType, mousePosition, selectedItem.SizeInTiles, ActivePlayerId);
     }
 
-    private Node2D InstantiateObjectForType(string objectType)
+    private Node2D InstantiateObjectForType(string objectType, Vector2I sizeInTiles)
     {
         if (ObjectPrefabs.TryGetValue(objectType, out var packedScene) && packedScene != null)
         {
             return packedScene.Instantiate<Node2D>();
         }
 
-        // Fallback marker to keep room layout loading functional even without configured prefabs.
-        return new Node2D { Name = objectType };
+        var node = new Node2D { Name = objectType };
+        var sprite = BuildItemSprite(objectType, sizeInTiles);
+        if (sprite != null)
+        {
+            node.AddChild(sprite);
+            return node;
+        }
+
+        var marker = new ColorRect
+        {
+            Color = new Color(0.2f, 0.75f, 1.0f, 0.65f),
+            Size = new Vector2(sizeInTiles.X * CellSize.X, sizeInTiles.Y * CellSize.Y),
+            Position = new Vector2(
+                -(sizeInTiles.X * CellSize.X) / 2.0f,
+                -(sizeInTiles.Y * CellSize.Y) / 2.0f
+            )
+        };
+        node.AddChild(marker);
+        return node;
     }
 
     private Vector2I GetGridPositionFromSnapped(Vector2 snappedPosition, Vector2I sizeInTiles)
@@ -280,5 +317,196 @@ public partial class RoomManager : Node2D
                 node.QueueFree();
             }
         }
+    }
+
+    private bool CanPlaceAt(Vector2I gridPosition, Vector2I sizeInTiles)
+    {
+        for (var x = 0; x < sizeInTiles.X; x++)
+        {
+            for (var y = 0; y < sizeInTiles.Y; y++)
+            {
+                var tile = new Vector2I(gridPosition.X + x, gridPosition.Y + y);
+                if (_occupiedTiles.Contains(tile))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private void MarkOccupied(Vector2I gridPosition, Vector2I sizeInTiles)
+    {
+        for (var x = 0; x < sizeInTiles.X; x++)
+        {
+            for (var y = 0; y < sizeInTiles.Y; y++)
+            {
+                _occupiedTiles.Add(new Vector2I(gridPosition.X + x, gridPosition.Y + y));
+            }
+        }
+    }
+
+    private void RebuildOccupiedTiles()
+    {
+        _occupiedTiles.Clear();
+        foreach (var child in ObjectsContainer.GetChildren())
+        {
+            if (child is not Node2D node)
+            {
+                continue;
+            }
+
+            var gridPosition = node.HasMeta("GridPosition")
+                ? (Vector2I)node.GetMeta("GridPosition")
+                : WorldToGrid(node.GlobalPosition);
+            var sizeInTiles = node.HasMeta("SizeInTiles")
+                ? (Vector2I)node.GetMeta("SizeInTiles")
+                : Vector2I.One;
+
+            MarkOccupied(gridPosition, sizeInTiles);
+        }
+    }
+
+    private void SetupInventory()
+    {
+        _inventoryItems.Clear();
+        _inventoryItems.Add(new InventoryItem
+        {
+            ObjectType = "Test_1x1",
+            TexturePath = "res://Asseti/sOBNI/test.png",
+            SizeInTiles = new Vector2I(1, 1)
+        });
+        _inventoryItems.Add(new InventoryItem
+        {
+            ObjectType = "Test_2x2",
+            TexturePath = "res://Asseti/sOBNI/test.png",
+            SizeInTiles = new Vector2I(2, 2)
+        });
+        _inventoryItems.Add(new InventoryItem
+        {
+            ObjectType = "Test_4x4",
+            TexturePath = "res://Asseti/sOBNI/test.png",
+            SizeInTiles = new Vector2I(4, 4)
+        });
+        _inventoryItems.Add(new InventoryItem
+        {
+            ObjectType = "Test_8x8",
+            TexturePath = "res://Asseti/sOBNI/test.png",
+            SizeInTiles = new Vector2I(8, 8)
+        });
+
+        _selectedItemIndex = 0;
+    }
+
+    private void BuildInventoryUi()
+    {
+        var canvasLayer = new CanvasLayer { Name = "InventoryUI" };
+        AddChild(canvasLayer);
+
+        var panel = new PanelContainer();
+        panel.SetAnchorsPreset(Control.LayoutPreset.TopLeft);
+        panel.Position = new Vector2(12, 12);
+        panel.Size = new Vector2(320, 140);
+        canvasLayer.AddChild(panel);
+
+        var vbox = new VBoxContainer();
+        panel.AddChild(vbox);
+
+        _inventoryLabel = new Label();
+        vbox.AddChild(_inventoryLabel);
+
+        var hintLabel = new Label
+        {
+            Text = "Click: place | 1-4/Tab: select | F5 save | F9 load"
+        };
+        vbox.AddChild(hintLabel);
+
+        var buttons = new HBoxContainer();
+        vbox.AddChild(buttons);
+
+        for (var i = 0; i < _inventoryItems.Count; i++)
+        {
+            var localIndex = i;
+            var item = _inventoryItems[i];
+            var button = new Button
+            {
+                Text = $"{i + 1}:{item.SizeInTiles.X}x{item.SizeInTiles.Y}",
+                CustomMinimumSize = new Vector2(70, 28)
+            };
+            button.Pressed += () => SelectInventoryItem(localIndex);
+            buttons.AddChild(button);
+        }
+
+        UpdateInventoryLabel();
+    }
+
+    private void SelectInventoryItem(int index)
+    {
+        if (index < 0 || index >= _inventoryItems.Count)
+        {
+            return;
+        }
+
+        _selectedItemIndex = index;
+        UpdateInventoryLabel();
+        var item = _inventoryItems[_selectedItemIndex];
+        GD.Print($"Selected item: {item.ObjectType} ({item.SizeInTiles.X}x{item.SizeInTiles.Y} tiles)");
+    }
+
+    private void UpdateInventoryLabel()
+    {
+        if (_inventoryLabel == null || _inventoryItems.Count == 0)
+        {
+            return;
+        }
+
+        var item = _inventoryItems[_selectedItemIndex];
+        _inventoryLabel.Text = $"Selected: {item.ObjectType} ({item.SizeInTiles.X}x{item.SizeInTiles.Y})";
+    }
+
+    private Sprite2D BuildItemSprite(string objectType, Vector2I sizeInTiles)
+    {
+        var texturePath = GetTexturePathForObjectType(objectType);
+        if (string.IsNullOrWhiteSpace(texturePath))
+        {
+            return null;
+        }
+
+        var texture = GD.Load<Texture2D>(texturePath);
+        if (texture == null)
+        {
+            return null;
+        }
+
+        var sprite = new Sprite2D
+        {
+            Texture = texture,
+            Centered = true
+        };
+
+        var targetSize = new Vector2(sizeInTiles.X * CellSize.X, sizeInTiles.Y * CellSize.Y);
+        if (texture.GetSize().X > 0 && texture.GetSize().Y > 0)
+        {
+            sprite.Scale = new Vector2(
+                targetSize.X / texture.GetSize().X,
+                targetSize.Y / texture.GetSize().Y
+            );
+        }
+
+        return sprite;
+    }
+
+    private string GetTexturePathForObjectType(string objectType)
+    {
+        foreach (var item in _inventoryItems)
+        {
+            if (item.ObjectType == objectType)
+            {
+                return item.TexturePath;
+            }
+        }
+
+        return string.Empty;
     }
 }
